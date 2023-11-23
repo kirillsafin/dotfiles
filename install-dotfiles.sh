5e397b5efc6d1f690c9c2f869d368c5c811447ab
MijikHna <kirill.safin@smail.th-koeln.de>
2023-11-23 13:07:09 +0100

    added vim-plug to install.sh

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
+# install pipx using apt if not exists
+[ ! -f '/usr/bin/pipx' ] && sudo apt install pipx -y || echo "pipx already exists"
 
-asfdasdf sf sdfa fa
+# install nodejs and npm using apt if not exists
+
+# install tmux if it doesn't exist
+[ ! -f '/usr/bin/tmux' ] && sudo apt install tmux -y || echo "tmux already exists"
+
+# create ~/prog/nvim if it doesn't exist
+mkdir -p ~/prog/nvim || echo "prog/nvim  already exists"
+
+# download nvim appimage app from https://github.com/neovim/neovim/releases/download/stable/nvim.appimage to ~/prog/nvim if it doesn't exist, make it executable, create a symlink to it in ~/local/bin
+[ ! -f "${HOME}/prog/nvim/nvim.appimage" ] && curl -L -o ~/prog/nvim/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage || echo "nvim.appimage already exists"
+chmod u+x ~/prog/nvim/nvim.appimage
+[ ! -f "${HOME}/.local/bin/nvim" ] && ln -sv ~/prog/nvim/nvim.appimage ~/.local/bin/nvim || echo "nvim symlink already exists"
+
+# install plug for nvim
+[ ! -f "${HOME}/.local/share/nvim/site/autoload/plug.vim" ] && curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install zsh and oh-my-zsh
+[ ! -f '/usr/bin/zsh' ] && sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# grep my theme from https://raw.githubusercontent.com/MijikHna/zeta-zsh-theme/master/zeta.zsh-theme
+curl https://raw.githubusercontent.com/MijikHna/zeta-zsh-theme/master/zeta.zsh-theme > ~/.oh-my-zsh/themes/zeta.zsh-theme
 
 ln -sv ~/.dotfiles/nvim ~/.config
 
